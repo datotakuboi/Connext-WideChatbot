2aac1a8383f9b5b14cb4c54472853c631a09296e
datotakuboi <aronetitong@gmailcom>
2024-07-28 11:14:28 +0800

    m

--- chatbot.py
+++ chatbot.py
@@ -219,7 +219,7 @@ def user_input(user_question, api_key):
 
 # Main app function
 def app():
-    st.set_page_config(page_title="Connext Chatbot", layout="wide")
+    
 
     # Retrieve API key from secrets
     google_ai_api_key = st.secrets["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"]
