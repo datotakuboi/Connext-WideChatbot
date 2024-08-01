19f7f3892253baa22c401669c6be3f4a533e8092
datotakuboi <aronetitong@gmailcom>
2024-08-01 20:49:30 +0800

    m

--- chatbot.py
+++ chatbot.py
@@ -29,6 +29,10 @@ if "conversation_history" not in st.session_state:
     st.session_state["conversation_history"] = []
 if "conversation_context" not in st.session_state:
     st.session_state["conversation_context"] = ""
+if "show_fine_tuned_expander" not in st.session_state:
+    st.session_state["show_fine_tuned_expander"] = False
+if "request_fine_tuned_answer" not in st.session_state:
+    st.session_state["request_fine_tuned_answer"] = False
 
 # Initialize Firebase SDK
 if not firebase_admin._apps:
@@ -270,7 +274,6 @@ def try_get_answer(user_question, context="", fine_tuned_knowledge = False):
 def user_input(user_question, api_key):
     
     with st.spinner("Processing..."):
-        st.session_state.show_fine_tuned_expander = True  # Reset
         embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
         new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
         docs = new_db.similarity_search(user_question)
@@ -327,9 +330,6 @@ def app():
 
     if "answer" not in st.session_state:
         st.session_state["answer"] = ""
-
-    if "request_fine_tuned_answer" not in st.session_state:
-        st.session_state["request_fine_tuned_answer"] = False
 
     if 'fine_tuned_answer_expander_state' not in st.session_state:
         st.session_state.fine_tuned_answer_expander_state = False
