380c7e00d50049d74efb8f3e17817eb5d8daef30
datotakuboi <aronetitong@gmailcom>
2024-08-03 13:56:19 +0800

    m

--- chatbot.py
+++ chatbot.py
@@ -292,10 +292,12 @@ def app():
     retrievers_ref = st.session_state.db.collection('Retrievers')
     docs = retrievers_ref.stream()
 
-    st.markdown("### Chat History")
-    for chat in st.session_state.chat_history:
-        st.write(f"**You:** {chat['user_question']}")
-        st.write(f"**Bot:** {chat['response']}")
+    chat_placeholder = st.empty()
+    with chat_placeholder.container():
+        st.markdown("### Chat History")
+        for chat in st.session_state.chat_history:
+            st.write(f"**You:** {chat['user_question']}")
+            st.write(f"**Bot:** {chat['response']}")
 
     user_question = st.text_input("Ask a Question", key="user_question")
     submit_button = st.button("Submit", key="submit_button")
@@ -366,26 +368,11 @@ def app():
     if submit_button:
         if user_question and google_ai_api_key:
             st.session_state.parsed_result = user_input(user_question, google_ai_api_key)
-
-    if st.session_state.parsed_result is not None and "Answer" in st.session_state.parsed_result:
-        st.markdown("### Reply:")
-        st.write(st.session_state.parsed_result['Answer'])
-
-        # Check if the answer is not directly in the context
-        if "Is_Answer_In_Context" in st.session_state.parsed_result and not st.session_state.parsed_result["Is_Answer_In_Context"]:
-            if st.session_state.show_fine_tuned_expander:
-                with st.expander("Get fine-tuned answer?", expanded=False):
-                    st.write("Would you like me to generate the answer based on my fine-tuned knowledge?")
-                    col1, col2, _ = st.columns([3,3,6])
-                    with col1:
-                        if st.button("Yes", key="yes_button"):
-                            st.session_state["request_fine_tuned_answer"] = True
-                            st.session_state.show_fine_tuned_expander = False
-                            st.rerun()
-                    with col2:
-                        if st.button("No", key="no_button"):
-                            st.session_state.show_fine_tuned_expander = False
-                            st.rerun()
+            with chat_placeholder.container():
+                st.markdown("### Chat History")
+                for chat in st.session_state.chat_history:
+                    st.write(f"**You:** {chat['user_question']}")
+                    st.write(f"**Bot:** {chat['response']}")
 
     if st.session_state["request_fine_tuned_answer"]:
         fine_tuned_result = try_get_answer(user_question, context="", fine_tuned_knowledge=True)
