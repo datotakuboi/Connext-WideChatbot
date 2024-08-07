2ede8a0c7b9be6a180d0a40526527b25ab9851a7
datotakuboi <aronetitong@gmailcom>
2024-08-08 00:33:10 +0800

    undo

--- chatbot.py
+++ chatbot.py
@@ -394,17 +394,13 @@ def app():
 
     display_chat_history()
 
-    if 'user_question' not in st.session_state:
-        st.session_state.user_question = ""
-
     user_question = st.text_area("Ask a Question", key="user_question")
     submit_button = st.button("Submit", key="submit_button")
     clear_history_button = st.button("Clear Chat History")
 
     if clear_history_button:
         st.session_state.chat_history = []
-        st.session_state.user_question = ""  # Clear the input field
-        st.experimental_rerun()
+        st.rerun()
 
     if "retrievers" not in st.session_state:
         st.session_state["retrievers"] = {}
@@ -418,12 +414,9 @@ def app():
             st.session_state.parsed_result = parsed_result
             if "Answer" in parsed_result:
                 st.session_state.chat_history.append({"question": user_question, "answer": parsed_result})
-                st.session_state.user_question = ""  # Clear the input field
-                st.experimental_rerun()  # Rerun to update the input field
+                display_chat_history()
             else:
                 st.toast("Failed to get a valid response from the model.")
-        else:
-            st.toast("Please enter a question.")
 
     display_chat_history()
 
